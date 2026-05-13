efb6f750168f68c15962f916eda99c5ab9f166f9
SAMMY-4870 <gayakesamarth@gmail.com>
2026-05-13 14:55:33 +0530

    updated full ci cd pipeline

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,21 +1,69 @@
 const admin =
+
   require("firebase-admin");
 
-const serviceAccount =
-  require("./service-account.json");
+// ======================================
+// FIREBASE INIT
+// ======================================
 
 if (!admin.apps.length) {
 
-  admin.initializeApp({
+  // LOCAL ENVIRONMENT
 
-    credential:
-      admin.credential.cert(
-        serviceAccount
-      )
-  });
+  if (
+
+    process.env.NODE_ENV !==
+
+    "production"
+
+  ) {
+
+    const serviceAccount =
+
+      require(
+        "./service-account.json"
+      );
+
+    admin.initializeApp({
+
+      credential:
+
+        admin.credential.cert(
+          serviceAccount
+        )
+    });
+
+    console.log(
+
+      "🔥 Firebase Local Connected"
+    );
+
+  }
+
+  // CLOUD RUN / GCP
+
+  else {
+
+    admin.initializeApp({
+
+      credential:
+
+        admin.credential.applicationDefault()
+    });
+
+    console.log(
+
+      "☁️ Firebase Cloud Connected"
+    );
+  }
 }
 
+// ======================================
+// FIRESTORE
+// ======================================
+
 const db =
+
   admin.firestore();
 
 module.exports = db;
